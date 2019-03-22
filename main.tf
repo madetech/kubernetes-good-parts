76f21f49112ade0515add00669663370d60910fe
Mark Sta Ana <booyaabooyaabooyaa@gmail.com>
2019-03-22 15:51:11 +0000

    initial commit :tada:

---
+++ main.tf
@@ -0,0 +1,51 @@
+provider "azurerm" {
+  version = "~>1.6"
+}
+
+################################################################################
+## Variables
+################################################################################
+variable "azure_client_id" {}
+variable "azure_client_secret" {}
+
+################################################################################
+## Resources
+################################################################################
+resource "azurerm_resource_group" "demo" {
+  name     = "acme-rg"
+  location = "UK South"
+}
+
+resource "azurerm_kubernetes_cluster" "demo" {
+  name                = "acme-demo"
+  location            = "${azurerm_resource_group.demo.location}"
+  resource_group_name = "${azurerm_resource_group.demo.name}"
+  dns_prefix          = "anvil"
+
+  kubernetes_version = "1.11.7"
+
+  linux_profile {
+    admin_username = "ubuntu"
+
+    ssh_key {
+      key_data = "${file("id_rsa_acme_k8s_demo.pub")}"
+    }
+  }
+
+  agent_pool_profile {
+    name            = "default"
+    count           = "1"
+    vm_size         = "Standard_F2s"
+    os_type         = "Linux"
+    os_disk_size_gb = "30"
+  }
+
+  service_principal {
+    client_id     = "${var.azure_client_id}"
+    client_secret = "${var.azure_client_secret}"
+  }
+
+  tags {
+    environment = "Global"
+  }
+}
